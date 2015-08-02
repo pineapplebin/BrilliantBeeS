c11b906e00698fc67aafeaa5709620e9b4ca3d14
pineapplebin <pineapplewing@126.com>
2015-08-03 00:02:25 +0800

    构建Thinkphp基础

---
+++ Application/Common/Common/function.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * 测试函数
+ * @param $arr
+ */
+function i_p($arr) {
+    echo '<br />'.$arr;
+}
---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,5 @@
+<?php
+$config = array(
+	//'配置项'=>'配置值'
+);
+return array_merge(include './conf.php', $config);
