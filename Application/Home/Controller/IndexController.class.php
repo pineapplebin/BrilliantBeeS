f89c6944254b553e40ba6c0a979f76a567b0260c
pineapplebin <pineapplewing@126.com>
2015-08-03 00:11:11 +0800

    modify something

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        echo C('ABC');
-        i_p('haha');
+        echo 'Hello world!';
     }
 }
