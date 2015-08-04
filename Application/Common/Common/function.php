13897a185b97d6561bad043ec8c4769094b45e3f
pineapplebin <pineapplewing@126.com>
2015-08-05 01:39:35 +0800

    增加flash功能，及使用示例SigninDemo

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -6,3 +6,35 @@
 function i_p($arr) {
     echo '<br />'.$arr;
 }
+
+/**
+ * 消息闪现函数
+ * 在控制器视图函数中以需要闪现的内容为参数调用此函数，
+ * 然后即可在下个显示的页面的模板中以get_flash_messages()函数显示闪现内容
+ *
+ * 例如，在注册页面中，后台处理后发现用户名重复，即可使用此函数，
+ * 并重定向至注册页面或其他页面，均能显示内容
+ *
+ * @param $msg 消息内容
+ */
+function flash($msg) {
+    $messages = session('flash_list');
+    if ($messages != null) {
+        $messages[] = $msg;
+    } else {
+        $messages = array($msg);
+    }
+    session('flash_list', $messages);
+}
+
+/**
+ * 获取闪现消息函数
+ * 在模板中调用此函数，并将得到的数组处理然后输出即可得到闪现消息
+ *
+ * @return 返回消息数组
+ */
+function get_flash_messages() {
+    $msg = session('flash_list');
+    session('flash_list', null);
+    return $msg;
+}
---
+++ Application/Home/Controller/SigninDemoController.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace Home\Controller;
+use Common\Controller\NormalBaseController;
+
+class SigninDemoController extends NormalBaseController {
+
+    public function index() {
+        $this->display();
+    }
+
+    public function handle() {
+        if (!IS_POST) {
+            $this->redirect('index');
+        }
+        $username = I('post.username');
+        $password = I('post.password');
+        // 检查两个是否填入，如果没有则闪现消息
+        $is_save = true;
+        if ($username == '') {
+            flash('请输入用户名');
+            $is_save = false;
+        }
+        if ($password == '') {
+            flash('请输入密码');
+            $is_save = false;
+        }
+        // 如果都输入了则返回save，否则重定向至注册页面
+        if ($is_save) {
+            echo 'save';
+        } else {
+            $this->redirect('index');
+        }
+    }
+}
