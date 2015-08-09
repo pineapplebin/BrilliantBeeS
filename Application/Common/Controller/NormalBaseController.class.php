e0ae3c00849e0908ef48df920d1d97cb2c554c42
pineapplebin <pineapplewing@126.com>
2015-08-09 20:24:13 +0800

    模板继承

--- Application/Common/Controller/NormalBaseController.class.php
+++ Application/Common/Controller/NormalBaseController.class.php
@@ -13,6 +13,6 @@ class NormalBaseController extends CommonController {
      * 所有派生自NormalBaseController的控制器都会先执行此方法内的内容
      */
     public function _initialize() {
-
+        $this->assign('show', 0);
     }
 }
---
+++ Application/Home/Controller/TestController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Home\Controller;
+use Common\Controller\NormalBaseController;
+
+class TestController extends NormalBaseController {
+
+    public function index() {
+        $this->display();
+    }
+}
