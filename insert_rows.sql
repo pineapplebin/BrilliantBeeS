68bf2f40d23f6769bbd01058049121eebc920dd9
kay <468862162@qq.com>
2015-08-17 09:37:31 +0800

    用户组管理

--- insert_rows.sql
+++ insert_rows.sql
@@ -1,11 +1,113 @@
 USE `brilliant_bee_s`;
 
 INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
-VALUES ('阅读帖子', 1);
+VALUES ('访问论坛', 1);
 
 INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
-VALUES ('发表评论', 1);
+VALUES ('阅读权限', 10);
 
 INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
-VALUES ('发表帖子', 1);
+VALUES ('隐身', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('使用搜索', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('自定义头衔', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发帖不受控制', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许发短消息', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许加好友', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('查看统计数据报表', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许使用应用', 1);
+
+-- 帖子相关
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发表话题', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发表回复', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发起投票', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('参与投票', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发表悬赏', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发表活动', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发表辩论', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发表交易', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许设置回帖奖励', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许使用标签', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('签名中使用编辑器代码', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('签名中使用[img]代码', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('主题评价影响值', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许参与评分', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许参与点评', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许使用多媒体代码', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('', 1);
+
+-- 家园相关
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许打招呼', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许表态', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('发表留言/评论', 1);
+
+-- 附件相关
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('下载附件', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('查看图片', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('上传附件', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('上传图片', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('允许设置附件权限', 1);
+
+INSERT INTO  `bbs_authority` (`auth_name`, `auth_level`) 
+VALUES ('附件类别', 1);
 
